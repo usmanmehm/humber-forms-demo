14a92a5726cd0bf1a2235d107570c532d1048627
Usman Mehmood <usman.mehm@outlook.com>
2022-06-04 14:30:24 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'form-demo';
+  form!: FormGroup;
+  ageArray!:any;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      csharp: new FormControl(''),
+      java: new FormControl(''),
+      javaScript: new FormControl(''),
+      favoriteFramework: new FormControl('')
+    })
+
+    const myArray = Array.from(Array(16).keys());
+    this.ageArray = myArray.map(el => el + 25);
+    console.log(this.ageArray);
+  }
+
+  getValues() {
+    const firstName = (this.form.get('randomControl') as FormControl)?.value;
+    const lastName = (this.form.get('lastName') as FormControl).value;
+    console.log('First name:', firstName);
+    console.log('Last name:', lastName);
+  }
+
+  populateValues() {
+    this.form.get('firstName')?.setValue('Jack');
+    this.form.get('lastName')?.setValue('Sparrow');
+  }
+
+  submit() {
+
+    console.log(this.form.valid);
+    
+  }
+
+  setSavedValues() {
+
+    this.form.get('firstName')?.value;
+    this.form.get('lastName')?.value;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileDataComponent } from './components/profile-data/profile-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileDataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
